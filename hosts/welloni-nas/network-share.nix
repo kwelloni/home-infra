1eb7384f4e390b7449b21b39084170f57a5a2119
kwelloni <kwells706@gmail.com>
2025-03-20 09:40:16 -0500

    Update config for samba

--- hosts/welloni-nas/network-share.nix
+++ hosts/welloni-nas/network-share.nix
@@ -4,20 +4,36 @@
   lib,
   ...
 }: {
+  systemd.tmpfiles.rules = [
+    "d /mnt/storage/share 0770 kevin users - -"
+  ];
   services.samba = {
     enable = true;
     openFirewall = true;
     settings = {
       global = {
+        "guest account" = "kevin";
+        "map to guest" = "Bad User";
+        "load printers" = "no";
+        "printcap name" = "/dev/null";
         "hosts allow" = "127.0.0.1 192.168.0.0/24";
         "hosts deny" = "0.0.0.0/0";
       };
-      storage = {
-        path = "/mnt/storage";
-        writeable = "true";
+      "share" = {
+        "path" = "/mnt/storage/share";
+        "guest ok" = "yes";
+        "read only" = "no";
         "create mask" = "0644";
         "directory mask" = "0755";
+        "force group" = "users";
       };
     };
   };
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
 }
